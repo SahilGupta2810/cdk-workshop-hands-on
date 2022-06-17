0327d146ee4327723a0a70994835c0014c11413e
Sahil Gupta <guptasahil2810@gmail.com>
2022-06-17 15:40:49 +0530

    "Initial Project Structure"

---
+++ test/cdk-workshop-hands-on.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkWorkshopHandsOn from '../lib/cdk-workshop-hands-on-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-workshop-hands-on-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkWorkshopHandsOn.CdkWorkshopHandsOnStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
